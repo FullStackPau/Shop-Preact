9aeadcce97d5db52ea01afe7e35a563c6496ab84
PauStack <paulopesruis@gmail.com>
2023-02-10 10:59:31 +0100

    Fixed Issue

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -6,28 +6,20 @@ import { HiShoppingCart } from "react-icons/hi";
 import style from "./style.css";
 
 const Header = () => {
-  const [showCart, setShowCart] = useState(false);
   return (
     <header>
       <span>INDITEX</span>
       <div className={style.shoppingcart}>
         <HiShoppingCart />
       </div>
-      {showCart ? (
-        <div className={style.listproducts}>
-          <div className={style.itemproductcart}>as</div>
-          <div className={style.itemproductcart}>as</div>
-          <div className={style.itemproductcart}>as</div>
-          <div className={style.itemproductcart}>as</div>
-          <div className={style.itemproductcart}>as</div>
-          <div className={style.itemproductcart}>as</div>
-          <div className={style.itemproductcart}>as</div>
-          <div className={style.itemproductcart}>as</div>
-          <div className={style.itemproductcart}>as</div>
-          <div className={style.itemproductcart}>as</div>
-          <div className={style.itemproductcart}>as</div>
-        </div>
-      ) : null}
+      <div className={style.listproducts}>
+        <div className={style.itemproductcart}>1</div>
+        <div className={style.itemproductcart}>2</div>
+        <div className={style.itemproductcart}>3</div>
+        <div className={style.itemproductcart}>4</div>
+        <div className={style.itemproductcart}>5</div>
+        <div className={style.itemproductcart}>6</div>
+      </div>
     </header>
   );
 };
